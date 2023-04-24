acd0df030085778eda5f22dbf3c7a87a0c2c0d23
Nick <nm9ur@virginia.edu>
2023-04-24 11:00:28 -0400

    test.sh now records bag files for failures

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@ for i in {0..49} ; do
     n=`expr $i \* 6` # 50 test BARN worlds with equal spacing indices: [0, 6, 12, ..., 294]
         for j in {1..5} ; do            
             # run the test
-            python3 run.py --world_idx $n
+            python3 run.py --world_idx $n --bag
             sleep 20
         done
 done
